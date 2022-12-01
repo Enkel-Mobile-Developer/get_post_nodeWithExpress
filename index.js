e0dba2ebc5a5be7449432147e561cbe5a3cfd646
GenjurX <118318434+GenjurX@users.noreply.github.com>
2022-12-01 12:56:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-/*Write a server that:
-- if client issue a GET /unique then it returns the unique values provided as “numbers” query parameter
-- if client issue a GET /sum then it returns the sum of all the values provided as “numbers” query parameter
-- if client issue a POST /unique then it returns the unique values provided as “numbers” post parameter
-- if client issue a POST /sum then it returns the sum of all the values provided as “numbers” post parameter*/
-
 import express from 'express';
 import {sumOfArray, returnUnique} from './functions.js';
 
 const app = express();
 app.use(express.json());
 
-// GET first part done
 app.get('/unique', async (req, res) => {
   console.log('GET method =>');
   const numbers= req.query.numbers;
   const unique = returnUnique(numbers);
   res.json(unique);
 });
-
-//
 
 app.get('/sum', async (req, res) => {
     console.log('GET method =>');
@@ -41,7 +32,7 @@ app.post('/unique', async (req, res) => {
   res.json(unique);
 });
 
-// POST
+
 app.post('/sum', async (req, res) => {
   console.log('POST method =>');
   const numbers = req.query.numbers;
